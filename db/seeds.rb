81fa56708e3108dd670f3f407559f17a18740e49
Pete Argent <peter.argent@gmail.com>
2013-01-01 13:47:15 +1100

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,6 @@ Role.create([
   { :name => 'VIP' }
 ], :without_protection => true)
 puts 'DEFAULT USERS'
-user = User.create! :name => 'Admin', :email => 'admin@example.com', :password => 'secret', :password_confirmation => 'secret', :slug => 'admin'
-puts 'user: ' << user.name
-user.add_role :admin
 user2 = User.find_or_create_by_email :name => 'Pete Argent', :email => 'pete@example.com', :password => 'secret', :password_confirmation => 'secret', :slug => 'second-user'
 puts 'user: ' << user2.name
 user2.add_role :admin
